efc116a39d7e66c710839cacbee2de4384a35911
Jonas Anderson <52059516+Jonas010@users.noreply.github.com>
2020-07-28 16:55:37 -0300

    melhorando o código.

--- ScriptGame.js
+++ ScriptGame.js
@@ -3,14 +3,34 @@ function jogo(fase) {
 
 	if (fase === 1){
 		NumAdvinha = prompt("vou pensar em um número de 1 á:")
+		
+		//apenas algumas brincadeiras para ficar mais divertido.
+		if (isNaN(numAdvinha)){
+			alert("apenas números.");
+			window.location.assign("game.html");
+		}
+		else if (numAdvinha == null) {
+			alert("tente outra vez.");
+			window.location.assign("game.html")
+		}
+		else if (numAdvinha <= 12) {
+			alert("Está testando o jogo?");
+		}
+		else if (numAdvinha <= 100) {
+			alert("Você tem coragem.");
+		}
+		else {
+			alert("hmm... um oponente a altura!");
+		}
 	}
+	
 	else if (fase === 2){
-		mense.innerHTML = "bora lá...";
-		number = Math.floor(Math.random()*NumAdvinha)+1
-	}
-	else if (fase === 3){
 		mense.innerHTML = "tente advinhar o número!";
+		document.querySelector('#next').innerHTML = "Proximo";
 		document.querySelector('h1').style.display="none";
+		
+		number = Math.floor(Math.random()*numAdvinha)+1 //para gerar o número aleatório
+		
 		entrada.setAttribute("style","display:block;");
 		entrada.value=null;
 		tentativas.style.marginTop="3%";
@@ -18,11 +38,12 @@ function jogo(fase) {
 	}
 	else {
 		var tenta = document.getElementById('entrada').value;
-		tentativas.innerHTML = fase-3;
+		tentativas.innerHTML = fase-2;
 
 		if (tenta == number) {
 			entrada.style.display="none";
 			mense.innerHTML = "acertou!";
+			mense.style.color = "green";
 			next.style.display="none";
 			document.querySelector('a').style.display="block";
 		}
