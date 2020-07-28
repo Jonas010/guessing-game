816f5dabb41213eb45a573ed3090a24dca89c38e
Jonas Anderson <52059516+Jonas010@users.noreply.github.com>
2020-07-28 17:33:12 -0300

    Update ScriptGame.js

--- ScriptGame.js
+++ ScriptGame.js
@@ -7,11 +7,11 @@ function jogo(fase) {
 
 		if (isNaN(numAdvinha)){
 			alert("apenas números.");
-			window.location.assign("jonas010.github.io/jogo-de-advinhar/");
+			window.location.assign("index.html");
 		}
 		else if (numAdvinha == null) {
 			alert("tente outra vez.");
-			window.location.assign("jonas010.github.io/jogo-de-advinhar/")
+			window.location.assign("index.html")
 		}
 		else if (numAdvinha <= 12) {
 			alert("Está testando o jogo?");
